3bec3da6cf9b7c6a737eef8b0409486d1f547456
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-19 20:10:56 +0500

    git commit "secondCommit_03"

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,37 @@ function App() {
     const [count, setCount] = useState(0)
       return(
           <>
-              <div>
-                  <h1>Try Programmer</h1>
-                  <Header/>
-                  <div>
-                      <h1>Counter: {count}</h1>
-                      <button onClick={() => setCount(count + 1)}>
-                          +
-                      </button>
-                      <button onClick={() => setCount(count - 1)}>
-                          -
-                      </button>
-                      <button onClick={() => setCount(count * 2)}>
-                          x
-                      </button>
+              <div className={"header"}>
+                  <div className={"header__title"}>
+                      <Header/>
+                      <h1>Try Programmer</h1>
+                  </div>
+                  <div className={"header__counter"}>
+                      <h1 className={"header__counter_main"}>Counter: <div className={"header__counter_title"}>{count}</div></h1>
+                          <button onClick={() => setCount(count + 1)}>
+                              +
+                          </button>
+                          <button onClick={() => setCount(count - 1)}>
+                              -
+                          </button>
+                          <button onClick={() => setCount(count * 2)}>
+                              x
+                          </button>
+                          <button onClick={() => setCount(count / 2)}>
+                              /
+                          </button>
+                          <button onClick={() => setCount(count * 0)}>
+                              0
+                          </button>
                   </div>
 
+
+
               </div>
+
+                  {/*<div id={"header_counter"}>*/}
+
+                  {/*</div>*/}
           </>
 
           )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
-import './/src/components/Header/Header.css';
+import './Header.css';
 function Header() {
     return <div>
         <h2><span>Architecture</span> Mode</h2>
-        <h2><span>Architecture</span> Mode</h2>
+
     </div>
 }
 
