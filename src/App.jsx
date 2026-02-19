11b8c24f59a485edf6b70d2bfe594ad81056bbc4
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-19 17:35:02 +0500

    secondCommit_02

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,9 @@ function App() {
                       <button onClick={() => setCount(count - 1)}>
                           -
                       </button>
+                      <button onClick={() => setCount(count * 2)}>
+                          x
+                      </button>
                   </div>
 
               </div>
