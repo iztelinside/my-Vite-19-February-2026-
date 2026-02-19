16a8652c2c3db03aaa8dff694a31a01d9e46f0d7
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-19 21:04:24 +0500

    git commit "thirdCommit_05"

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,75 @@ import { useState } from "react";
 import Header from "./components/Header/Header.jsx";
 
 function App() {
-    const [name, setName] = useState("")
-    const [count, setCount] = useState(0)
+    const [users, setUsers] = useState([
+        {id: 1, name: "Dan"},
+        {id: 2, name: "Alex"}
+    ])
+
+    const [newUser, setNewUser] = useState("")
+    // const [name, setName] = useState("")
+    // const [count, setCount] = useState(0)
+    const addUser = () => {
+        const newItem = {
+            id: Date.now(),
+            name: newUser
+        }
+
+
+        setUsers([...users, newItem])
+        setNewUser("")
+}
+    const deleteUser = (id) => {
+        const filteredUsers = users.filter(user => user.id !== id)
+        setUsers(filteredUsers)
+    }
 
       return(
           <>
-              <div>
-                  <input
-                      type="text"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                  />
+          <div>
+            <h1>User List</h1>
 
-                  <h1>Hello, {name}</h1>
+            <input
+              type="text"
+              value={newUser}
+              onChange={(e) => setNewUser(e.target.value)}
+            />
+           <button onClick={addUser}>Add</button>
+           {/*<button onClick={deleteUser}>Delete</button>*/}
+
+
+
+           <ul>
+              {users.map((user) => (
+               <li key={user.id}>
+
+                  {user.name}
+                   {/*<button onClick={() => addUser(user.id)}>*/}
+                   {/*    Add*/}
+                   {/*</button>*/}
+                   <button onClick={() => deleteUser(user.id)}>
+                       Delete
+                   </button>
+
+               </li>
+           ))}
+          </ul>
+                  {/*<input*/}
+                  {/*    type="text"*/}
+                  {/*    value={name}*/}
+                  {/*    onChange={(e) => setName(e.target.value)}*/}
+                  {/*/>*/}
+
+                  {/*<h1>Hello, {name}</h1>*/}
 
 
 
 
-                  <h2>Counter: {count}</h2>
-                  <button onClick={() => setCount(count + 1)}>+</button>
-                  <button onClick={() => setCount(count - 1)}>-</button>
-                  <button onClick={() => setName("")}>Clear</button>
-              </div>
+                  {/*<h2>Counter: {count}</h2>*/}
+                  {/*<button onClick={() => setCount(count + 1)}>+</button>*/}
+                  {/*<button onClick={() => setCount(count - 1)}>-</button>*/}
+                  {/*<button onClick={() => setName("")}>Clear</button>*/}
+          </div>
           </>
           // <>
           //     <div className={"header"}>
