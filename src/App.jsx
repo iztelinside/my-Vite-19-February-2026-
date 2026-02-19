3f492d6fd0bbdacb008954fa0292cdac97c1954e
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-19 16:54:18 +0500

    git status

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import './App.css';
+import { useState } from "react";
 import Header from "./components/Header/Header.jsx";
 
 function App() {
+    const [count, setCount] = useState(0)
       return(
           <div>
+            <h1>Try Programmer</h1>
             <Header/>
             <h1>Try Programmer</h1>
-             <h1>Try Programmer</h1>
+
           </div>
           )
 }
