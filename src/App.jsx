28373f05727da66a6d0c676544b07bc6641922b1
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-19 17:17:07 +0500

    commit firstt

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import Header from "./components/Header/Header.jsx";
 function App() {
     const [count, setCount] = useState(0)
       return(
-          <div>
-            <h1>Try Programmer</h1>
-            <Header/>
-            <h1>Try Programmer</h1>
-            <div>
-               <h1>Counter: {count}</h1>
-               <button onClick={() => setCount(count + 1)}>
-                   +
-               </button>
-            </div>
+          <>
+              <div>
+                  <h1>Try Programmer</h1>
+                  <Header/>
+                  <h1>Try Programmer</h1>
+                  <div>
+                      <h1>Counter: {count}</h1>
+                      <button onClick={() => setCount(count + 1)}>
+                          +
+                      </button>
+                  </div>
 
-          </div>
+              </div>
+          </>
+
           )
 }
 
