91619e4390e0512c85f3c8212b959f1e365fc0c4
Daniyar Izteleuov <d.iztel@gmail.com>
2026-02-19 16:12:56 +0500

    git remote add origin https://github.com/iztelinside/vite-js-19-02-2026-javascript-project.git
    third03 commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import './/src/components/Header/Header.css';
+function Header() {
+    return <div>
+        <h2><span>Architecture</span> Mode</h2>
+        <h2><span>Architecture</span> Mode</h2>
+    </div>
+}
+
+export default Header
